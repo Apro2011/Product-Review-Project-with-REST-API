4186e03972135408e83eafae4bf1e8537020e908
Aprotim Biswas <biswasaprotim2011@outlook.com>
2023-03-05 22:12:31 +0530

    Product Review Backend

---
+++ reviews/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Product, Category, Company, ProductSize, ProductSite, Comment, Image
+from django.contrib.auth.models import Group
+
+# Register your models here.
+
+# admin.register() decorator
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "content")
+    list_filter = ("category",)
+
+
+admin.site.register(Category)
+admin.site.register(Company)
+admin.site.register(ProductSize)
+admin.site.register(ProductSite)
+admin.site.register(Comment)
+admin.site.register(Image)
+admin.site.site_header = "Product Review Admin"
+
+admin.site.unregister(Group)
